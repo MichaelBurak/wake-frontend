3b937fe5aaf12ed7bc0fe3c23ba625f39006da6e
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2021-01-23 07:34:44 -0700

    rizzo o giggle

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="background-color:#222C3C;" >
+    <gb-heading weight="light" tag="h1">Oh A Lot Of Fun At Finnegans Wake
+      <gb-icon name="book"></gb-icon>
+    </gb-heading>
+    <gb-divider color="blue" theme="dark"></gb-divider>
+    <gb-avatar size="huge" src="https://cdn.theculturetrip.com/wp-content/uploads/2015/06/screen-shot-2016-09-28-at-4-56-55-pm.png"></gb-avatar>
+    <gb-checkbox size="large" description="Activate 'Night Mode' -- more creative text" v-model="checkBox" ></gb-checkbox>
+    <gb-textarea size="small" 
+    value="bar"
+    
+    v-model="prefix"
+    ></gb-textarea>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods:{
+    // check(){
+    //   console.log(`checked is ${this.checkBox}`)
+    // }
+  },
+  data(){
+    return {
+      checkBox: false,
+    prefix: "Make your words tidal to the mascarete here(and there and where you fare)" 
+    
+}
+  }
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import Vue from 'vue'
 import App from './App.vue'
 
+import Vue from "vue";
+import VueDarkMode from "@growthbunker/vuedarkmode";
+
+Vue.use(VueDarkMode,{theme: "dark"});
 Vue.config.productionTip = false
 
 new Vue({
