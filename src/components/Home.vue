d6612f9b4400080c69957e8158345aa98fddeb4a
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2021-01-23 10:18:05 -0700

    little icon change the vibes are good

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div style="background-color:#222C3C;" >
+    
     <gb-heading weight="light" tag="h1">Oh A Lot Of Fun At Finnegans Wake
-      <gb-icon name="book"></gb-icon>
+      <gb-icon name="nights_stay"></gb-icon>
     </gb-heading>
     <gb-divider color="blue" theme="dark"></gb-divider>
     <gb-avatar size="huge" src="https://cdn.theculturetrip.com/wp-content/uploads/2015/06/screen-shot-2016-09-28-at-4-56-55-pm.png"></gb-avatar>
-    <gb-checkbox size="large" description="Activate 'Night Mode' -- more creative text" v-model="checkBox" ></gb-checkbox>
+    <!-- <gb-checkbox size="large" status="success" description="Activate 'Night Mode' -- more creative text" v-model="checkBox" ></gb-checkbox> -->
     <gb-textarea size="small" 
     value="bar"
     
@@ -26,7 +27,7 @@ export default {
   data(){
     return {
       checkBox: false,
-    prefix: "Make your words tidal to the mascarete here(and there and where you fare)" 
+    prefix: "Don't be absintheminded, make yourself tidal to join in the mascarete. O gig goggle of gigguels. I can't tell you how! (Type in the box and submit to generate text)" 
     
 }
   }
